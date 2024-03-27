974ee228e50dd8c063e8ed17b386214c9d1ae7c1
Tsire <152901801+Tsireledzo-P@users.noreply.github.com>
2024-03-27 10:29:51 +0200

    Add files via upload
    practical 2

---
+++ Array.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		string names[] = { "Tsireledzo", "Vuyo", "Tamia", "Allen", "Lutho" };
+		Console.WriteLine(names[0]);
+		//an array of names
+	}
+}
